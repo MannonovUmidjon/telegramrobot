65e675bca1b1f325e572b5e013f2866c6a19d8a3
MannonovUmidjon <umidjonmannonov39@gmail.com>
2025-05-27 23:17:31 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 BOT_TOKEN = "7348821242:AAEYxu9eXAJjV9mbovPkYO5oaLQBMeRNmwo"
-ADMIN_IDS = [7646506662]  # Admin Telegram ID ro'yxati
+ADMIN_IDS = [7646506662]  # O'zingizning Telegram ID'nizni shu yerga yozing
