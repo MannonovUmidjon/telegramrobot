041b4e1a523930f3621e0af6fb63c5b187b3568e
MannonovUmidjon <umidjonmannonov39@gmail.com>
2025-05-28 01:06:25 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "7348821242:AAEUanyPSffmdAeMUBZ40ZnP0Vo0kYABbvA"
+BOT_TOKEN = "7348821242:AAEYxu9eXAJjV9mbovPkYO5oaLQBMeRNmwo"
 ADMIN_IDS = [7646506662]  # O'zingizning Telegram ID'nizni shu yerga yozing
---
+++ start_bot.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\Users\user\Desktop\multilang_telegram_bot_final
+start python main.py
+exit
