c917fab74c86bdbc9dfeadc8bb55a6245b39c956
MannonovUmidjon <umidjonmannonov39@gmail.com>
2025-05-29 23:51:06 +0500

    main.py

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from flask import Flask, request
+import telebot
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_TOKEN = 'YOUR_BOT_TOKEN'  # ← bu yerga tokeningizni yozing
+bot = telebot.TeleBot(API_TOKEN)
 
-# FUNKSIYALARGA TEGILMADI
-def start(update, context):
-    update.message.reply_text("Botga xush kelibsiz!")
+app = Flask(__name__)
 
-def help_command(update, context):
-    update.message.reply_text("Yordam uchun bu yerda man.")
+# Webhook qabul qiluvchi marshrut
+@app.route('/', methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return '', 200
+    else:
+        return 'Invalid content type', 403
 
-def echo(update, context):
-    update.message.reply_text(update.message.text)
+# Test sahifa (majburiy emas, faqat tekshirish uchun)
+@app.route('/')
+def index():
+    return "Bot ishlayapti!"
 
-def main():
-    updater = Updater(BOT_TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    # Qo‘shilgan handlerlar (aslidagi funksiyalar)
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help_command))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Botni ishga tushurish
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app.run()
