9614833122eb160f437935d77ddb7cb84d96dbc6
MannonovUmidjon <umidjonmannonov39@gmail.com>
2025-06-02 00:12:50 +0500

    config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "7348821242:AAESEiuDFKWaq3L0NxJinRmACvDaFHMXHIY"
+BOT_TOKEN = "{7348821242:AAGb8Drd7fbCXjK8fvmdrYLFqAYIOUCY5l8"
 ADMIN_IDS = [7646506662]  # O'zingizning Telegram ID'nizni shu yerga yozing
